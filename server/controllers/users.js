bde0e2f1b73b1f4d976d1e1de12773c7db976004
sohaaaa123 <144840285+sohaaaa123@users.noreply.github.com>
2023-12-03 01:50:43 -0500

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,9 @@
-let express = require('express');
-let router = express.Router();
+let express = require('express');  // Import the Express.js framework
+let router = express.Router();    // Create an instance of the Express router
 
-module.exports.displayUsersPage = (req, res, next)=>{
+// Exporting a function named 'displayUsersPage' as part of the module.exports object
+module.exports.displayUsersPage = (req, res, next)=>{   
+  // Rendering the 'index' view/template with specified data
+  // The data object here includes a 'title' property set to 'Users'
   res.render('index', { title: 'Users' });
 }
